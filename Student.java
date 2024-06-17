752b219695f014d5607b7d81863a4d15b213cd7e
Ayush Tiwari <ayushtiwari615@gmail.com>
2024-06-18 00:27:45 +0530

    added student class

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    int rollNumber;
+    String name;
+    int age;
+    int class;
+
+    Student(String name, int age, int class, int rollNumber)
+    {
+        this.name = name;
+        this.age = age;
+        this.class = class;
+        this.rollNumber = rollNumber;
+    }
+}
